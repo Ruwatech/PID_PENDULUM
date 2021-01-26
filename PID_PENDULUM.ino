839c99329ed79a9eeb45a7e01cf49642e1b6b615
Ruwatech <ruwatech@live.com>
2021-01-26 07:59:36 +0530

    Add files via upload

---
+++ PID_PENDULUM.ino
@@ -0,0 +1,66 @@
+/*
+  +PID_PENDULUM DEMO by MyhubEDU Sri Lanka.
+  +Special thanks for Jeff Rowberg for providing the awesome I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP.
+  +Big Applause to the arduino community in Sri Lanka & in the World.
+  
+*/
+
+void setup() {
+ initMPU();
+}
+
+void loop() {
+ scanMPU();   
+}
+
+
+
+double prevErr = 0;
+double totErr = 0;
+int kp = 5;
+int kd = 10;
+float ki = 0.01;
+
+
+void pidAlgo(double sY) {
+  
+  double sp = -1 * map(analogRead(A0), 0, 1023, 0, 90); //Get the set point from the potentiometer
+  double error = sp - sY; //obtain the error from substracting
+
+  double Pterm = error * kp;
+  double Dterm = (error - prevErr) * kd;
+  double Iterm = (double)ki * totErr;
+
+  double pidVal =  Pterm + Dterm + Iterm ; 
+
+  int mspeed = (int)pidVal;
+  drM(-mspeed);
+
+  prevErr = error;
+  totErr += error;
+  Serial.print(sp);
+  Serial.print(",");
+  Serial.print(sY);
+  Serial.print(",");
+  Serial.println(pidVal);
+}
+
+int En = 5;
+int Pp = 6;
+int Np = 7;
+void drM(int m1) {
+  if (m1 >= 255) {
+    m1 = 255;
+  }
+  if (m1 <= -255) {
+    m1 = -255;
+  }
+  if (m1 < 0) {
+    m1 = -1 * m1;
+    digitalWrite(Pp, LOW); digitalWrite(Np, HIGH);
+    analogWrite(En, m1);
+  } else {
+    digitalWrite(Pp, HIGH); digitalWrite(Np, LOW);
+    analogWrite(En, m1);
+  }
+}
---
+++ easyMPU.ino
@@ -0,0 +1,126 @@
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+#define BUTTON_PIN 15
+#define LED_PIN A0
+int ledState = LOW;
+int buttonState;
+int lastButtonState = LOW;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+bool blinkState = false;
+
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+boolean initB = true;
+float initX;
+float initR;
+float initY;
+
+float sX;
+float sR;
+float sY;
+
+float lastX;
+float lastR;
+float lastY;
+
+void pidAlgo(int sY);
+void initMPU() {
+  Wire.begin();
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+  pinMode(A0, INPUT);
+  if (devStatus == 0) {
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+}
+
+void scanMPU() {
+  if (!dmpReady) return;
+  while (!mpuInterrupt && fifoCount < packetSize) {}
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+  fifoCount = mpu.getFIFOCount();
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    mpu.resetFIFO();
+  } else if (mpuIntStatus & 0x02) {
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    fifoCount -= packetSize;
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+
+    if (initB) {
+      initB = false;
+      sX = ypr[0] * 180 / M_PI;
+      sR = ypr[1] * 180 / M_PI;
+      sY = ypr[2] * 180 / M_PI;
+      initX = sX;
+      initR = sR;
+      initY = sY;
+    } else {
+      sX = (ypr[0] * 180 / M_PI) - initX;
+      sR = (ypr[1] * 180 / M_PI) - initR;
+      sY = (ypr[2] * 180 / M_PI) - initY;
+      pidAlgo(sY);
+    }
+
+
+  }
+
+}
